96d502488dc2cba890d1b5afde82d370765b2cbb
dinusha <dinusha@wso2.com>
2015-06-14 00:26:21 +0530

    Apps-Created-Over_time graph

---
+++ portal/store/gadget/Apps_Created_Over_Time/js/gadgetconf.js
@@ -0,0 +1,1 @@
+var gadgetConfig ={"id":"Apps_Created_Over_Time","title":"Apps Created Over Time","datasource":"TMP1","type":"batch","columns":[{"name":"id","type":"INTEGER"},{"name":"cicy","type":"STRING"}],"maxUpdateValue":10,"chartConfig":{"chartType":"bar","xAxis":1,"yAxis":0}};
